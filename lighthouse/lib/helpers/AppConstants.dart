a6bfff4853e9811710cdb8f3ab4777fdd46bd1da
Dmytro Fedoruk <dmytro.fedoru@gmail.com>
2023-03-23 00:57:30 +0500

    Revert "-- changes"
    This reverts commit 9eaf8463cadeaf9dc641e7e1378e669d780471aa.

--- lighthouse/lib/helpers/AppConstants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String tokenKey = 'token';
-  static const String refreshtokenKey = 'resfreshtoken';
-  static const String userNameKey = 'userName';
-  static const String libreTokenKey = 'libretoken';
-  static const String librePatientId = 'librePatientId';
-}
